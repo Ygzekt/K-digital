842e60eeb271afd08526a6ca4e4bdbf3b7190ca5
minyeong <109118029+Ygzekt@users.noreply.github.com>
2022-07-26 18:48:22 +0900

    Add files via upload

---
+++ 220726백준/9498번.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf_s("%d", &a);
+	if (90 <= a) 
+	{
+		printf("A\n");
+	}
+	else if (80 <= a) 
+	{
+		printf("B\n");
+	}
+	else if (70 <= a) 
+	{
+		printf("C\n");
+	}
+	else if (60 <= a) 
+	{
+		printf("D\n");
+	}
+	else 
+	{
+		printf("F\n");
+	}
+
+	return 0;
+}
